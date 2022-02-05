385b2d17848ded57a8e1aaf8bbd8bc02b93723fc
Ray <72323408+Mrlatosta@users.noreply.github.com>
2022-02-04 22:50:12 -0500

    ScrollFade added

---
+++ index.js
@@ -0,0 +1,76 @@
+
+	window.sr = ScrollReveal();
+
+		sr.reveal('.navbar', {
+				duration: 1000,
+				origin: 'bottom',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#bgText', {
+				duration: 2000,
+				origin: 'top',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#bgText', {
+				duration: 2000,
+				origin: 'top',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#ti1', {
+				duration: 1000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+
+		sr.reveal('#ti2', {
+				duration: 2000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+
+		sr.reveal('#ti3', {
+				duration: 3000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#pr-1', {
+				duration: 1000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#pr-2', {
+				duration: 1000,
+				origin: 'left',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#conten', {
+				duration: 1000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
